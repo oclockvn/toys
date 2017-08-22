d975a1d448b68bc99908bd740b88ad9fb95c6de9
oclockvn <oclockvn@gmail.com>
2017-08-22 15:00:48 +0700

    added hash helper

---
+++ toys/Extensions/NumberExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toys.Extensions
+{
+    public static class NumberExtensions
+    {
+        /// <summary>
+        /// Determine a string is number or not
+        /// </summary>
+        /// <param name="s">The string to check</param>
+        /// <returns>True if is a number. Otherwise return false</returns>
+        public static bool IsNumber(this string s)
+        {
+            if (string.IsNullOrEmpty(s))
+                return false;
+
+            return s.Length > 0 && s.All(char.IsDigit);
+        }
+    }
+}
